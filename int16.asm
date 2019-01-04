3bd8ed603f5d3235953b09c305eaf427eb7e8371
Michael Chourdakis <m@turboirc.comc>
2019-01-04 14:18:24 +0200

    More dmmi

--- int16.asm
+++ int16.asm
@@ -1,23 +1,109 @@
 ; --------------------------------------- int 0xF0 real ---------------------------------------
+
+c16o dw 0
+c16s dw 0
+
+
+Thread16C:
+
+	thread16header STACK16T1,stack16t1_end
+
+	mov ax,CODE16
+	mov ds,ax
+    mov ax,0x25F0
+	mov dx,int16
+	int 0x21
+	
+
+	mov ax,CODE16
+	mov ds,ax
+	call far dword [c16o]
+	cli
+	hlt
+	hlt
+
+
 int16:
 
 	; AX 0, find interface
 	cmp ax,0
 	jnz .n0
+		push ds
+		mov ax,DATA16
+		mov ds,ax
+		mov dl,[numcpus]
+		pop ds
 		mov ax,0xFACE
 	IRET
 .n0:
 
-	; AX 8, find num of cous
-	cmp ax,8
-	jnz .n8
-		push ds
-		mov ax,DATA16
-		mov ds,ax
-		mov al,[numcpus]
-		pop ds
-	IRET
-.n8:
+	; AH 1, begin thead
+	cmp ah,1
+	jnz .n1
 
+		; BL = CPU
+		; AL = 0 = Unreal mode thread
+		; ES:DX = Run address
+
+		and ebx,0xFF
+		mov ax,CODE16
+		mov ds,ax
+		mov [c16s],es
+		mov [c16o],dx
+		linear eax,Thread16C,CODE16
+		call far CODE16:SendSIPIf
+	IRET
+
+
+.n1:
+
+
+
+	; AH 5, mutex functions
+	cmp ah,5
+	jnz .n5
+
+		; Initialize mutex
+		cmp al,0
+		jnz .n50
+			mov byte [es:di],0xFF
+		iret
+		.n50:
+
+		; lock mutex
+		cmp al,2
+		jnz .n52
+			dec byte [es:di]
+		iret
+		.n52:
+
+		; unlock mutex
+		cmp al,3
+		jnz .n53
+			cmp byte [es:di],0xFF
+			jz .okl
+				inc byte [es:di]
+			.okl:
+		iret
+		.n53:
+
+		; wait mutex
+		cmp al,4
+		jnz .n54
+			
+			.Loop1:		
+			CMP byte [es:di],0xff
+			JZ .OutLoop1
+			pause 
+			JMP .Loop1
+			.OutLoop1:
+
+		iret
+		.n54:
+
+	IRET
+
+
+.n5:
 
 IRET
--- main.asm
+++ main.asm
@@ -1,6 +1,30 @@
+
+m1 db "Hello",0xd,0xa,"$";
+mut1 db 0
+
+rt1:
+
+
+sti
+push cs
+pop ds
+mov dx,m1
+mov ax,0x0900
+int 0x21
+
+
+; unlock mut
+push cs
+pop es
+mov di,mut1
+mov ax,0x0503
+int 0xF0
+
+retf
+
+
 main:
 
-break16
 mov ax,0
 int 0xF0
 cmp ax,0xFACE
@@ -8,7 +32,51 @@ jz .y
 retf
 
 .y:
-mov ax,8
+; dl = num of cpus
+
+; init mut
+push cs
+pop es
+mov di,mut1
+mov ax,0x0500
+int 0xF0
+
+; lock mut 
+push cs
+pop es
+mov di,mut1
+mov ax,0x0502
+int 0xF0
+
+; lock mut 
+push cs
+pop es
+mov di,mut1
+mov ax,0x0502
+int 0xF0
+
+; run a thread
+push cs
+pop es
+mov dx,rt1
+mov ax,0x0101
+mov ebx,1
+int 0xF0
+
+; run a thread
+push cs
+pop es
+mov dx,rt1
+mov ax,0x0101
+mov ebx,2
+int 0xF0
+
+
+; wait mut
+push cs
+pop es
+mov di,mut1
+mov ax,0x0504
 int 0xF0
 
 retf
